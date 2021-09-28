e7b00f11cd38fd8e25a66d4168a052ac80e4cdd1
Nguyễn Quang Thanh <80471767+QuangThanhn485@users.noreply.github.com>
2021-09-28 17:57:13 +0700

    up code

---
+++ time/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_servertime = new System.Windows.Forms.Label();
+            this.btnTime = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_servertime
+            // 
+            this.lbl_servertime.AutoSize = true;
+            this.lbl_servertime.Location = new System.Drawing.Point(84, 58);
+            this.lbl_servertime.Name = "lbl_servertime";
+            this.lbl_servertime.Size = new System.Drawing.Size(46, 17);
+            this.lbl_servertime.TabIndex = 0;
+            this.lbl_servertime.Text = "label1";
+            // 
+            // btnTime
+            // 
+            this.btnTime.Location = new System.Drawing.Point(87, 117);
+            this.btnTime.Name = "btnTime";
+            this.btnTime.Size = new System.Drawing.Size(75, 23);
+            this.btnTime.TabIndex = 1;
+            this.btnTime.Text = "button1";
+            this.btnTime.UseVisualStyleBackColor = true;
+            this.btnTime.Click += new System.EventHandler(this.btnTime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTime);
+            this.Controls.Add(this.lbl_servertime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_servertime;
+        private System.Windows.Forms.Button btnTime;
+    }
+}
+
---
+++ time/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace time
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTime_Click(object sender, EventArgs e)
+        {
+            lbl_servertime.Text = ServerTime.getServerTime().ToLocalTime().ToString();
+        }
+        internal class ServerTime
+        {
+            private static string[] servers = { "132.163.96.1", "132.163.96.2", "129.6.15.28", "129.6.15.29" };
+
+
+            public static DateTime getServerTime()
+            {
+                foreach (string server in servers)
+                {
+                    DateTime result = connectServer(server);
+                    if (result > DateTime.MinValue)
+                        return result;
+                }
+                return DateTime.MinValue;
+            }
+
+            private static DateTime connectServer(string host)
+            {
+                string time;
+                try
+                {
+                    StreamReader read = new StreamReader(new TcpClient(host, 13).GetStream());
+                    DateTime systemTime = DateTime.UtcNow;
+                    time = read.ReadToEnd();
+                }
+                catch (Exception)
+                {
+                    return DateTime.MinValue;
+                }
+
+                if (time.Substring(38, 9) != "UTC(NIST)") return DateTime.MinValue;
+                if (time.Substring(30, 1) != "0") return DateTime.MinValue;
+
+                int jd = int.Parse(time.Substring(1, 5));
+                int yr = int.Parse(time.Substring(7, 2));
+                int mo = int.Parse(time.Substring(10, 2));
+                int dy = int.Parse(time.Substring(13, 2));
+                int hr = int.Parse(time.Substring(16, 2));
+                int mm = int.Parse(time.Substring(19, 2));
+                int sc = int.Parse(time.Substring(22, 2));
+
+
+                if (jd < 15020) return DateTime.MinValue; //trước năm 1900
+
+                if (jd > 51544)
+                    yr += 2000;
+                else
+                    yr += 1900;
+
+                return new DateTime(yr, mo, dy, hr, mm, sc);
+            }
+        }
+    }
+}
